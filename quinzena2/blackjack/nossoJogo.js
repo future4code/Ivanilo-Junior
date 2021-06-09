9b793c7fb8249fe8cb0b448756851e43a731f068
Ivanilo Junior <ivanilojunior@live.com>
2021-06-09 11:07:16 -0300

    Projeto Quinzenal2

---
+++ quinzena2/blackjack/nossoJogo.js
@@ -0,0 +1,31 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+console.log("Bem vindo ao jogo de Blackjack!")
+    if (confirm("Iniciar uma nova rodada?")){
+    }else{
+       console.log("O jogo acabou.")
+    }
+
+   function baralho(numero,nipe){
+   const J = 10
+   const Q = 10
+   const K = 10
+   const A = 11
+   const cartas = {
+   numeros: [1,2,3,4,5,6,7,8,9,,10,J,Q,K,A]
+   }
+   const nipes = {
+      nipes:["♠","♣","♥","♦"]
+   }
+}
+console.log(carta.texto)
