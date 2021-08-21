684a3f11fe63d0cca23c95388cd65892d8a5a380
Camila Azevedo Santos <ivanilojuior@live.com>
2021-08-21 16:32:59 -0300

    terceiro commit

--- labefy/labefy/src/components/ListaDetalhada.js
+++ labefy/labefy/src/components/ListaDetalhada.js
@@ -5,7 +5,10 @@ export default class ListaDetalhada extends React.Component{
 state={
     lista:[],
     musicas:[],
-    ids:''
+    ids:'',
+    InputMusica:'',
+    InputArtista:'',
+    InputUrl:''
 }
 componentDidMount(){
     this.pegarPlaylists()
@@ -31,14 +34,29 @@ componentDidUpdate(){
             this.setState({musicas:response.data.result.tracks,ids:id})
         })
     }
-    onClickAdcionar = () =>{
-        const pergunta = prompt('Deseja adcionar uma musica a sua playlist?')
-            if(pergunta==='sim'){
-               const nomeMusica= prompt('Nome da Musica:')
-               const artistaMusica = prompt('Nome do Artista/Banda:')
-               const urlMusica = prompt('Link da Musica:')
+    onClickAdcionar = (id) =>{
+        let pergunta = window.confirm('Deseja adcionar uma musica a playlist?')
+            if(pergunta===true){
+               const nomeMusica= prompt('Nome da Musica:');
+               const artistaMusica = prompt('Nome do Artista/Banda:');
+               const urlMusica = prompt('Link da Musica:');
+               this.setState({InputMusica:nomeMusica,InputArtista:artistaMusica,InputUrl:urlMusica});
+               axios.post(`https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${id}/tracks`,{
+                   'name':this.state.InputMusica,
+                   'artist':this.state.InputArtista,
+                   'url':this.state.InputUrl,
+               },{
+                   headers:{
+                       Authorization:'ivanilo-junior-johnson'
+                   }
+               }).then((response)=>{
+                   alert('Musica adcionada com sucesso!')
+               }).catch((error)=>{
+                   alert(error)
+               })
             }
     }
+
 
     render(){
         const printMusicas = this.state.musicas.map((item,i)=>{
@@ -47,10 +65,10 @@ componentDidUpdate(){
         const printLista = this.state.lista.map((item,i)=>
         {{return <li key={i}>{item.name}
         <button onClick={()=>{{this.pegarMusicas(item.id)}}}>^</button>
-        <button onClick={this.onClickAdcionar}>+</button>
-        {this.state.ids==item.id ?<p>{printMusicas}</p>:undefined}
+        <button onClick={()=>{{this.onClickAdcionar(item.id)}}}>+</button>
+        {this.state.ids===item.id ?<p>{printMusicas}</p>:undefined}
         </li>}})
-
+console.log(this.state)
         return(
             <div>
                 {printLista}
